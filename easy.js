4e69ab039ae94e9b87251ac0219592b408297daa
RenanDev2578 <158621298+RenanDev2578@users.noreply.github.com>
2024-09-16 16:33:28 -0300

    Initial commit with project files

---
+++ easy.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById("login").addEventListener("click", function() {
+    window.location.href = "login.html"; 
+});
+
+document.getElementById("adtarefas").addEventListener("click", function() {
+    window.location.href = "easyadtarefas.html"; 
+});
+
+
+
